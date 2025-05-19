31245c0d81fd42ecd27fbdc66ce5aca9c5f729c6
Damsiha Kumarasinghe <damsihak@gmail.com>
2025-05-19 14:31:06 +0530

    Create CandidateRepository

--- src/main/java/org/damsi/recruitmentpipelineapi/repository/CandidateRepository.java
+++ src/main/java/org/damsi/recruitmentpipelineapi/repository/CandidateRepository.java
@@ -1,4 +1,13 @@
 package org.damsi.recruitmentpipelineapi.repository;
 
-public class CandidateRepository {
+import org.damsi.recruitmentpipelineapi.model.ApplicationStage;
+import org.damsi.recruitmentpipelineapi.model.Candidate;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CandidateRepository extends MongoRepository<Candidate, String> {
+    List<Candidate> findByApplicationStage(ApplicationStage applicationStage);
 }
